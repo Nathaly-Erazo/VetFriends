d4cdc00bdfb1f78be59a445f7fe2ff51a7d1b718
Nathaly <nataerazo96@gmail.com>
2023-02-12 12:20:20 +0100

    Eventos añadidos

---
+++ js/sobre_nosotros.js
@@ -0,0 +1,13 @@
+function abrirAccordion(item) {
+    
+    var panel = item.lastElementChild;
+    panel.style.display = "block";
+}
+
+
+function cerrarAccordion(item) {
+
+    var panel = item.lastElementChild;
+    panel.style.display = "none";
+}
+
